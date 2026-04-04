a57d8050b1f1d093974fb779e775068ec7f24d10
____e_vil <69020282+VillaramaEnzo@users.noreply.github.com>
2026-04-04 16:33:50 +1300

    Create loveyou.tsx

---
+++ blank/app/ily/loveyou.tsx
@@ -0,0 +1,4 @@
+// My lovely girlfriend's birthday is coming up
+// I want to do something special for her (especially given the long distance)
+// So I want to make a passowrd protected portion of my website dedicated to her. This way only she who has the passowrd can view it. 
+// I have less that 2 weeks to build this. Should be fine right
